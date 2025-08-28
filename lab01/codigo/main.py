8a00de42969a4a229d87d88ad1b641ec76952fac
xMarih <marry-29@hotmail.com>
2025-08-27 21:28:00 -0300

    modularização

--- lab01/codigo/main.py
+++ lab01/codigo/main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from services.git import GitHubService
 from typing import Dict, List
 from collections import defaultdict
 
 from services.save_to_csv import SaveToCSV
 from services.print_summary import CalculateMetrics
+
 
 class MetricsAnalyzer:
     def __init__(self):
@@ -32,6 +36,7 @@ def main():
     df = pd.read_csv(csv_path)
     repositories: List[Dict] = df.to_dict(orient='records')
 
+    print(f"Total repositories loaded: {len(repositories)}")
     # Salva resumo em Markdown em relatorios/
     CalculateMetrics.print_summary(repositories, os.path.join(base_dir, 'lab01s01_summary.md'))
 
